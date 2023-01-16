66bec9aa2c0fda16a0f914bacedb5b5352211e54
Zicka67 <art.book@hotmail.fr>
2023-01-16 13:58:46 +0100

    Création du dossier JS

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    let list = document.querySelector("ul")
+
+    let listElements = list.querySelectorAll("li")
+
+    listElements.forEach(function(element)
+    {
+        element.style.color = "red"
+    })
+
